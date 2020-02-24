e4da25d177a919ca1b4b2fd16b4e628f934cd790
Erika M <erikam@localhost.localdomain>
2020-02-23 19:23:33 -0600

    me arrepentí de lo que hice

--- src/conversiones/DiezADiesciseis.java
+++ src/conversiones/DiezADiesciseis.java
@@ -9,15 +9,7 @@ public class DiezADiesciseis implements IConversion{
 
     @Override
     public String convertir(String num){
-	 int d = Integer.parseInt(num);
-    String digitos = "0123456789ABCDEF";
-    if (d <= 0) return "0"; //no es un entero 
-    String hexa = "";
-    while (d > 0) {
-        int digit = d % 16; //el digito más a a derecha
-        hexa = digitos.charAt(digit) + hexa;  //concatenación 
-        d = d / 16;
-    }
-    return hexa;
+	 
+    return null;
     }
 }
